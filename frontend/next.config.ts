524cc32cd08f35690b258995122ad3d038b17eff
am-david-jiang <jiangqiang2009@126.com>
2026-04-24 03:05:35 +0800

    chore(frontend): modify api base url in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    // NEXT_PUBLIC_API_BASE: "https://activity-management-system-server.vercel.app/api/",
-    // NEXT_PUBLIC_WS_URL: "https://activity-management-system-server.vercel.app",
-    NEXT_PUBLIC_API_BASE: "http://localhost:8000/api/",
-    NEXT_PUBLIC_WS_URL: "ws://localhost:8000",
+    NEXT_PUBLIC_API_BASE: "https://davidjiang.tech/ams-demo/api/",
+    NEXT_PUBLIC_WS_URL: "wss://davidjiang.tech",
   },
 };
 
